ce2bd3a0a7b8a3df1ae82ca43858fa15bc802e84
nataly.o <natalyorona@gmail.com>
2020-03-01 18:21:19 -0300

    Menu de navegación

---
+++ app/src/main/java/com/SM/sm/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package com.SM.sm.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Búsqueda de Profesionales y Servicios");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
